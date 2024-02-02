51acd7065261ec55bfb21ec81d90f5120786e7d7
jo17-dev <procompter1@gmail.com>
2024-02-02 12:44:04 -0500

    refactorisation du low inv container

--- src/components/Itemstat.tsx
+++ src/components/Itemstat.tsx
@@ -1,10 +1,11 @@
 const Itemstat = (props: any)=>{
+
     return (
         <div className={'w-16 mx-auto ' + props?.containerClass}>
             <img src={props?.imgurl} alt="img" className={"rounded-full w-16 h-16" + props?.imgClass} />
-            <div className="flex flex-row items-end mt-4">
-                <div className="bg-red-500 h-10 w-6 text-white text-center font-bold">4</div>
-                <div className="bg-gray-800 h-6 w-6 text-white text-center font-bold">8</div>
+            <div className="flex flex-row items-end mt- h-10">
+                <div className={"bg-red-500 h-"+props.inventorylevel+" w-6 text-white text-center font-bold ml-auto"}> {props.inventorylevel} </div>
+                <div className={"bg-gray-800 h-"+props.avgsales+" w-6 text-white text-center font-bold mr-auto"}>{props.avgsales} </div>
             </div>
             <p className="text-sm font-semibold"> {props?.itemName || 'Blue shirt'} </p>
             <button className="mt-2 w-full text-sm text-white rounded-xl border border-red-500 bg-red-500 hover:bg-white hover:text-red-500">order</button>
---
+++ src/containers/Lowinventory.tsx
@@ -0,0 +1,46 @@
+import Itemstat from "../components/Itemstat";
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa6";
+
+const Lowinventory = () =>{
+    const ItemStatsContainer = () => {
+        const items =[];
+        for(let i=0; i<7;i++){
+            items.push(<Itemstat imgurl="/src/assets/avatar.png" inventorylevel={Math.ceil(Math.random()*10)} avgsales={Math.ceil(Math.random()*10)} />);
+        }
+        return (
+        <>
+            {items}
+        </>
+        )
+    }
+
+
+    return(
+        <div className="mt-4">
+        <div className=" w-100 flex flex-row h-8 justify-between">
+            <span className="font-bold text-base" >Low inventory <span className="font-normal text-sm" >Less than 10 items</span>
+            </span>
+            <div className="mr-2">
+                <select name="low_inventory_sorting" id="inventory_sorting" className="text-sm">
+                    <option value="default">Sort By</option>
+                    <option value="lowest">Lowest</option>
+                    <option value="bigger">Bigger</option>
+                </select>
+                <input type="text" name="low_inventory_search" id="low_inventory_search" placeholder="Search" className=" ml-2 border-b border-gray-400" />
+            </div>
+        </div>
+        <div className="flex flex-row">
+            <FaAngleLeft className="text-black w-10 h-10 hover:text-red-500 m-0 my-auto mr-2" />
+            <ItemStatsContainer />
+            <FaAngleRight className="text-black w-10 h-10 hover:text-red-500 m-0 my-auto mr-2" />
+        </div>
+        <div className="mt-2 mx-auto w-full flex flex-row justify-center">
+            <p className="mx-4 text-sm"> <span className=" inline-block w-4 h-4 rounded-full bg-red-500"></span> Inventory level</p>
+            <p className="mx-4 text-sm"> <span className=" inline-block w-4 h-4 rounded-full bg-gray-500"></span> Dialy sales averages</p>
+        </div>
+    </div>
+    )
+}
+
+export default Lowinventory
